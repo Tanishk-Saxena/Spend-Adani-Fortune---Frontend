2e058f784111daa33d0fb553c7cb6c5e3d1b7dae
Tanishk Saxena <tanishksaxena686@outlook.com>
2022-10-11 00:58:19 +0530

    added basic css styles and made more responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import image from '../gautam-adani.jpg';
+import image from '../images/gautam-adani.jpg';
+import { useSelector } from 'react-redux';
 
 
 const Header = () => {
+
+  const balance = useSelector(state => state.balance);
+
   return (
     <header>
        <img src={image} alt="" />
@@ -10,7 +14,7 @@ const Header = () => {
        <h1 className="title">Spend Gautam Adani's Fortune!</h1>
 
        <div className='about'>If Mr. Gautam Adani cashed out all of his stocks & assets today he would have
-approximately <strong style={{fontWeight: "900", color: "rgba(0,0,0,0.9)"}}>₹92,00,00,00,00,000</strong> (9.2 Lakh Crore Indian Rupees) in his bank account.</div>
+approximately <strong style={{fontWeight: "900", color: "rgba(0,0,0,0.9)"}}>₹{balance.toLocaleString()}</strong> (11 Lakh Crore Indian Rupees) in his bank account.</div>
 
         <div className="challenge">
             <p>If you had that much money, how would <strong style={{fontWeight: "900", color: "rgba(0,0,0,0.9)"}}>you spend it</strong>?</p>
