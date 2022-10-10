529983572c90bff3e535a2ad2cbf1a469cfc51a0
Tanishk Saxena <tanishksaxena686@outlook.com>
2022-10-11 00:58:01 +0530

    further incorporated redux states

--- src/components/BalanceBar.js
+++ src/components/BalanceBar.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const BalanceBar = () => {
+
+  const balance = useSelector(state => state.balance);
+  const expense = useSelector(state => state.expense);
+
   return (
     <div className='balance-bar '>
-        <div className='balance-remaining'>Remaining: ₹92,00,00,00,00,000</div>
-        <div className='balance-spent'>You haven't spent a single ruppee yet! Start Buying!</div>
+        <div className='balance-remaining'>{`Remaining: ₹${balance.toLocaleString()}`}</div>
+        <div className='balance-spent'>{expense===0?"You haven't spent a single ruppee yet! Start Buying!":`You only spent ${(expense/110000000000).toFixed(8)} % of the total!`}</div>
     </div>
   )
 }
