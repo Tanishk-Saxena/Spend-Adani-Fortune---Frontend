b0bf1253f3c8959d71b43222bbb731d609655624
Tanishk Saxena <tanishksaxena686@outlook.com>
2022-10-11 00:56:31 +0530

    added automated items and image inclusion from db

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux/es/exports';
+import { actionCreators } from '../state/index'
+import { bindActionCreators } from 'redux';
 
-const Item = ({image, name, price, state, setState}) => {
+const Item = ({image, name, cost, setRestart, restart}) => {
+  
+  const [state, setState] = useState(0);
+  const dispatch = useDispatch();
+  const {buy, sell} = bindActionCreators(actionCreators, dispatch);
+  useEffect(() => {
+    if(restart===true){
+      setState(0);
+    }
+  }, [restart])
   return (
     <>
     <div className='item'>
-        <img src={image} alt="" />
+        <img src={require(`../images/${image}.jpg`)} alt="image description" />
         <p className="name">{name}</p>
-        <p className="price">{price}</p>
+        <p className="price">₹ {cost.toLocaleString()}</p>
         <div className='button-row'>
-            <div className='button sell'>Sell</div>
+            <div className='button sell' onClick={()=>{if(state!==0){setState(state-1);sell(cost);}}}>Sell</div>
             <p>{state}</p>
-            <div className='button buy'>Buy</div>
+            <div className='button buy' onClick={()=>{setRestart(false);setState(state+1);buy(cost);}}>Buy</div>
         </div>
     </div>
     </>
--- src/components/ItemContainer.js
+++ src/components/ItemContainer.js
@@ -1,23 +1,41 @@
-import React from 'react'
-import Item from './Item'
-import image from '../gautam-adani.jpg'
-
-const setState = () => {}
+import React, {useState, useEffect} from 'react';
+import { useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionCreators } from '../state';
+import Item from './Item';
 
 const ItemContainer = () => {
+
+  const [restart, setRestart] = useState(true);
+  const [items, setItems] = useState([]);
+  const items_url = 'http://localhost:5555/get_items';
+
+  const fetchItems = async () => {
+    let data = await fetch(items_url);
+    data = await data.json();
+    setItems(data);
+  }
+
+  useEffect(() => {
+    fetchItems();
+  }, [])
+  
+  const dispatch = useDispatch();
+  const {clear} = bindActionCreators(actionCreators, dispatch);
+
   return (
+    <>
     <div className="item-container">
-        <Item image={image} name={"name"} price={"price"} state={0} setState={setState}/>
-        <Item image={image} name={"name"} price={"price"} state={0} setState={setState}/>
-        <Item image={image} name={"name"} price={"price"} state={0} setState={setState}/>
-        <Item image={image} name={"name"} price={"price"} state={0} setState={setState}/>
-        <Item image={image} name={"name"} price={"price"} state={0} setState={setState}/>
-        <Item image={image} name={"name"} price={"price"} state={0} setState={setState}/>
-        <Item image={image} name={"name"} price={"price"} state={0} setState={setState}/>
-        <Item image={image} name={"name"} price={"price"} state={0} setState={setState}/>
-        <Item image={image} name={"name"} price={"price"} state={0} setState={setState}/>
-        <Item image={image} name={"name"} price={"price"} state={0} setState={setState}/>
+      {items.length>0 && items.map((item, index)=>{
+        let cost_in_rs = item.cost * 82.40;
+        let rounded_off_cost = Math.round(cost_in_rs/1000)*1000;
+        return(
+          <Item restart={restart} setRestart={setRestart} key={index} image={`item${index+1}`} name={item.name} cost={rounded_off_cost}/>
+        );
+      })}
     </div>
+    <button className='clear-btn' onClick={()=>{clear(); setRestart(true);}}>Clear Purchases</button>
+    </>
   )
 }
 
