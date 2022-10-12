63e001517ef39d4b0aeb11a13af0dd56dd8502a4
Tanishk Saxena <tanishksaxena686@outlook.com>
2022-10-13 02:57:28 +0530

    created receipt template

---
+++ src/components/Template.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Template = React.forwardRef((props, ref) => {
+  const { receipt, expense } = props;
+  let date = new Date();
+  return (
+    <div ref={ref} style={{display: "flex", flexDirection: "column", justifyContent: "space-between", height: "290mm", padding: "10px"}}>
+        <div style={{display: "flex", flexDirection: "column"}}>
+            <header className="print_header">
+                <h1 className="receipt_title">Spend Adani's Fortune</h1>
+                <div style={{display: "flex", justifyContent: "space-between", width: "210mm"}}>
+                    <h4>{date.toLocaleString()}</h4>
+                    <h4>Spend Adani's Fortune</h4>
+                </div>
+            </header>
+            <div className="print_body">
+                <h1>Receipt</h1>
+                {
+                    receipt.map((item, index) => {
+                    if(item.freq > 0)
+                        return <h5 className="receipt-record" key={index}>{item.name} x {item.freq}</h5>
+                    })
+                }
+                <h4 className="receipt-total">Total: ₹ {expense.toLocaleString()}</h4>
+            </div>
+        </div>
+        <footer className="print_footer">
+            <h4>Made By: Tanishk</h4>
+        </footer>
+    </div>
+  )
+})
+
+export default Template
